761c89dfd4572189a15b40295ffec1ad0d8c0f68
saili21 <saili.jawale@synerzip.com>
2021-10-03 15:10:24 +0530

    Added Search by name and sort by name

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,16 @@
 import React, { useEffect } from "react";
 import { useHomePage } from "../hooks/useHomePage";
 import PokemonCard from "./PokemonCard";
-import { Button, Grid, Typography } from "@mui/material";
+import {
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 export default function HomePage() {
   const {
@@ -9,10 +18,32 @@ export default function HomePage() {
     getNextPokemonList,
     getPreviousPokemonList,
     pokemonList,
+    numberOfCards,
+    setNumberOfCards,
+    sortBy,
+    setSortBy,
+    searchPokemons,
+    sortPokemonBy,
   } = useHomePage();
   useEffect(() => {
     getPokemonList();
   }, []);
+
+  useEffect(() => {
+    if (numberOfCards) {
+      getPokemonList(numberOfCards);
+    }
+  }, [numberOfCards]);
+
+  const handleChange = (event) => {
+    setNumberOfCards(event.target.value);
+  };
+
+  const handleSort = (event) => {
+    setSortBy(event.target.value);
+    sortPokemonBy();
+  };
+
   return (
     <React.Fragment>
       <Grid container item justifyContent="center" style={{ marginBottom: 20 }}>
@@ -25,6 +56,48 @@ export default function HomePage() {
           spacing={2}
           style={{ backgroundColor: "#D3D3D3", padding: 20 }}
         >
+          <Grid container item xs={12} spacing={3}>
+            <Grid container item xs={3}>
+              <TextField
+                onChange={searchPokemons}
+                label="Search by name and abilities"
+              ></TextField>
+            </Grid>
+            <Grid container item xs={2}>
+              <FormControl fullWidth>
+                <InputLabel id="sort-by-label">Sort By</InputLabel>
+                <Select
+                  labelId="sort-by-label"
+                  id="sort-by-select"
+                  value={sortBy}
+                  label="Sort By"
+                  onChange={handleSort}
+                >
+                  <MenuItem value={"name"}>Name</MenuItem>
+                  <MenuItem value={"height"}>Height</MenuItem>
+                  <MenuItem value={"weight"}>Weight</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={3}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">
+                  Select number of cards
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={numberOfCards}
+                  label="Select number  of cards"
+                  onChange={handleChange}
+                >
+                  <MenuItem value={10}>10</MenuItem>
+                  <MenuItem value={20}>20</MenuItem>
+                  <MenuItem value={50}>50</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
           <Grid
             container
             item
@@ -45,7 +118,7 @@ export default function HomePage() {
             </Button>
           </Grid>
           {pokemonList.results.map((data) => (
-            <Grid item xs={12} md={4} lg={3}>
+            <Grid key={data.name} item xs={12} md={4} lg={3}>
               <PokemonCard name={data.name} detailsURL={data.url} />
             </Grid>
           ))}
--- src/hooks/useHomePage.js
+++ src/hooks/useHomePage.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 import { useState } from "react";
-
+import { cloneDeep } from "lodash";
 export const useHomePage = () => {
   const [pokemonList, setPokemonList] = useState(null);
   const [pokemonDetails, setPokemonDetails] = useState(null);
+  const [numberOfCards, setNumberOfCards] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
 
-  const getPokemonList = () => {
+  const getPokemonList = (limit = 20) => {
     axios
-      .get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
+      .get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=0`)
       .then(function (response) {
         console.log("Pokemon List:", response.data);
         if (response.status === 200) {
@@ -77,6 +79,36 @@ export const useHomePage = () => {
       });
   };
 
+  const searchPokemons = (event) => {
+    let _pokemonList = cloneDeep(pokemonList);
+
+    if (event.target.value) {
+      let searchedPokemonList = pokemonList.results.filter((ele) =>
+        ele.name.includes(event.target.value)
+      );
+      _pokemonList.results = searchedPokemonList;
+      setPokemonList(_pokemonList);
+    } else {
+      numberOfCards ? getPokemonList(numberOfCards) : getPokemonList();
+    }
+  };
+
+  const sortPokemonBy = () => {
+    let _pokemonList = cloneDeep(pokemonList);
+
+    _pokemonList.results.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+
+    setPokemonList(_pokemonList);
+  };
+
   return {
     getPokemonList,
     getPokemonDetails,
@@ -84,5 +116,11 @@ export const useHomePage = () => {
     getNextPokemonList,
     pokemonList,
     pokemonDetails,
+    numberOfCards,
+    setNumberOfCards,
+    sortBy,
+    setSortBy,
+    searchPokemons,
+    sortPokemonBy,
   };
 };
